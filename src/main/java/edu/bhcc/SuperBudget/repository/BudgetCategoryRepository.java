95b3ca3ac26ec8f1dd95896dff16d5231d6613fc
mandip <mandip.amgain123@gmail.com>
2024-05-28 20:09:15 -0400

    new updates

--- src/main/java/edu/bhcc/SuperBudget/repository/BudgetCategoryRepository.java
+++ src/main/java/edu/bhcc/SuperBudget/repository/BudgetCategoryRepository.java
@@ -4,7 +4,6 @@ package edu.bhcc.SuperBudget.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import edu.bhcc.SuperBudget.model.BudgetCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface BudgetCategoryRepository extends JpaRepository<BudgetCategory, Long> {
